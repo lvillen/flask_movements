00e08aab2fada360517159b7e53b4a51c65b91a2
Luis Villén <l.villenjaldon@gmail.com>
2020-12-04 00:33:52 +0100

    Version 0.1

--- movements/views.py
+++ movements/views.py
@@ -1,6 +1,20 @@
 from movements import app
 from flask import render_template
+import csv
 
 @app.route('/')
-def listaMovimientos():
-    return render_template('movementsList.html')
+def listaIngresos():
+    fIngresos = open("movements/data/basededatos.csv", "r")
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
+    ingresos = list(csvReader)
+    total=0
+    for ingreso in ingresos:
+        total += float(ingreso[2])
+
+    #print(ingresos)
+    
+    return render_template('movementsList.html', datos=ingresos, total=total)
+
+    @app.rout('/creaalta')
+        def nuevoIngreso():
+            return 'Ya vemos cómo lo haremos'
